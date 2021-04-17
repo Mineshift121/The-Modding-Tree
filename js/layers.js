be3197a43f085861488a789cbc564a42b5a9d751
Mineshift121 <82699662+Mineshift121@users.noreply.github.com>
2021-04-17 16:47:18 +0200

    added a layer
    testing

--- js/layers.js
+++ js/layers.js
@@ -26,3 +26,37 @@ addLayer("p", {
     ],
     layerShown(){return true}
 })
+
+
+
+addLayer("o", {
+    name: "Omega", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "O", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+    color: "#4BDC13",
+    requires: new Decimal(1e50), // Can be a function that takes requirement increases into account
+    resource: "Omegas", // Name of prestige currency
+    baseResource: "prestige points", // Name of resource prestige is based on
+    baseAmount() {return player.prestige points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 1.5, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(3)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(3)
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "o", description: "O: Reset for Omegas", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return true}
+})
+
+
+
